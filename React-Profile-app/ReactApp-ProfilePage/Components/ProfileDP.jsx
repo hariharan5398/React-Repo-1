818ec8101d4010f8f3d6326f0bb2e64e5b2472d7
Hari Hara Suthan S <hariharan5398@gmail.com>
2022-12-15 18:02:04 +0000

    last Version in Netlify

--- React-Profile-app/ReactApp-ProfilePage/Components/ProfileDP.jsx
+++ React-Profile-app/ReactApp-ProfilePage/Components/ProfileDP.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import displayPic from '../Images/react-display-picture-1.png'
 import './components.css'
 
 function ProfilePicture(){
     return(
         <div className='dp-section'>
-        <img src="../Images/react-display-picture-1.png" alt="display picture" className='display-pic'/>
+        <img src={displayPic} alt="display picture" className='display-pic'/>
         </div>
     )
 }
