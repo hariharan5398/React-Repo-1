680788b01ca2a8d30dc496433b2cb1dfa0a45364
Hari Hara Suthan S <hariharan5398@gmail.com>
2022-12-20 19:30:32 +0000

    1st version

--- React-TravelJournal-App/public/data.js
+++ React-TravelJournal-App/public/data.js
@@ -16,7 +16,7 @@ export default [
             googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
             startDate: "27 May, 2021",
             endDate: "8 Jun, 2021",
-            description: "The SYdney Opera House is a multi -venue performing arts centre in Sydney. Located on the banks of the sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+            description: "The Sydney Opera House is a multi -venue performing arts centre in Sydney. Located on the banks of the sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
             imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
         },
 
