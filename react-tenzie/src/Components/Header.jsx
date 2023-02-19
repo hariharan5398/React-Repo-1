61f3b238795204407a6630a6982fe91015edb876
Hari Hara Suthan S <hariharan5398@gmail.com>
2023-02-19 02:52:32 +0000

    Tenzies
    To Be updated

--- react-tenzie/src/Components/Header.jsx
+++ react-tenzie/src/Components/Header.jsx
@@ -6,11 +6,11 @@ export default function Header(){
 
 
     return (
-        <div>
-            <h1 className='card-title'>
+        <div className='TileSpace col-7 col-md-5 col-lg-3'>
+            <h1>
                 Tenzies
             </h1>
-            <p className='card-text'>
+            <p>
                 Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
             </p>
 
