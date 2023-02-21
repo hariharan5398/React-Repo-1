b86c07031ceb1992e2bd0f5355e13ae4fd35a763
Hari Hara Suthan S <hariharan5398@gmail.com>
2023-02-21 16:35:41 +0000

    Update Quiz.jsx
    V2

--- react-quizzical/src/assets/Components/Quiz.jsx
+++ react-quizzical/src/assets/Components/Quiz.jsx
@@ -6,17 +6,15 @@ export default function Quiz(){
     // API Used
     // https://opentdb.com/api_config.php
     const [ quesArray, setQuesArray ] = React.useState(()=>[])
+    const blockedAnswer = React.useRef({})
+    const correctAnswer = React.useRef({})
+    let mark = 0;
+    
 
     // Fetch data from Api only on the initial render
     console.log(quesArray)
     React.useEffect(
-        // ()=>{
-        //     fetch("https://opentdb.com/api.php?amount=10")
-        // .then(response => response.json())
-        // .then(data => quesArray = [...data.results])
-        // console.log("temp", quesArray)
-        // setTimeout(()=>console.log(quesArray), 5000)
-        // },[]
+       
 
         ()=>{
             
@@ -25,8 +23,8 @@ export default function Quiz(){
                     console.log(1)
                 let response = await fetch("https://opentdb.com/api.php?amount=10")
                 let data = await response.json()
-                console.log(data.results)
                 setQuesArray([...data.results])
+                
             }
 
             )()
@@ -35,7 +33,7 @@ export default function Quiz(){
 
     )
 
-    // console.log(quesArray)
+    
 
     // Get the randomindex to insert right option in options array
     function getRandomIndex(){
@@ -43,18 +41,16 @@ export default function Quiz(){
     }
 
 
-    function blockAnswer(e){
+    function onChangeAns(e){
         
-        e.target.parentNode.classList.add("bg-blue-500")
-        e.target.parentNode.classList.add("text-white")
-
-        
+        blockedAnswer.current[e.target.name] = e.target.value
+        // console.log(blockedAnswer)
     }
 
     // Merge both correct & incorrectoption create an array
     function options(element, key){
         let optionArray = [...element.incorrect_answers]
-
+        correctAnswer.current[key] = element.correct_answer
         //Inserts correct ansewer in the random index
         optionArray.splice(getRandomIndex(), 0 , element.correct_answer)
         return (
@@ -64,9 +60,10 @@ export default function Quiz(){
 
                     return (
                         <div key={key+index} >
-                        <input className='hidden peer' key={key} type="radio" id={optionValue+key} value={optionValue} name={key}/>
-                        <label htmlFor={optionValue+key} className="cursor-pointer bg-green-500"   >
-                        <div className='m-2 border-2 border-blue-600 rounded-full px-2'>{optionValue}</div>
+                        
+                        <label htmlFor={optionValue+key} className="cursor-pointer"   >
+                        <input className='hidden peer' key={key} type="radio" id={optionValue+key} value={optionValue} name={key} onClick={(e)=>onChangeAns(e)} />
+                        <div className='m-2 border-2 border-blue-600 rounded-full px-2 peer-checked:font-bold peer-checked:bg-blue-600 peer-checked:text-white'>{optionValue}</div>
                         
                         </label>
                         </div>
@@ -76,6 +73,25 @@ export default function Quiz(){
         )
 
     }
+
+    function evaluate(){
+
+        if(Object.keys(blockedAnswer.current).length == quesArray.length){
+
+            for (let key of Object.keys(blockedAnswer.current)){
+
+                if(blockedAnswer.current[key] == correctAnswer.current[key]){
+                    mark+=1
+                }
+            }
+            console.log(mark)
+        }
+
+        
+
+    }
+
+
 
     let ques = quesArray.map(
         (element)=>{
@@ -95,8 +111,9 @@ export default function Quiz(){
     )
 
     return(
-        <div>
+        <div className=' w-screen'>
             <h1>{ques}</h1>
+            <button className=' my-4 mx-auto px-2 border-2 rounded-full bg-blue-500 text-white text-lg' onClick={()=>evaluate()}>{ques ? "Submit" : ""} </button>
         </div>
     )
 }
